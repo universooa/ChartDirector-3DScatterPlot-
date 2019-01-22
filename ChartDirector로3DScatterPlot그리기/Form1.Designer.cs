fa77425df87f3bc4cbdd702d9bd29a4bd3c8a93c
universooa <41477328+universooa@users.noreply.github.com>
2019-01-22 10:19:41 +0900

    csv파일읽고 3d surface chart그리기

--- ChartDirector로3DScatterPlot그리기/Form1.Designer.cs
+++ ChartDirector로3DScatterPlot그리기/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.winChartViewer1 = new ChartDirector.WinChartViewer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.DrawFrameOnRotate = new System.Windows.Forms.CheckBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.PointerPB = new System.Windows.Forms.RadioButton();
+            this.zoomInPB = new System.Windows.Forms.RadioButton();
+            this.zoomOutPB = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.winChartViewer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // winChartViewer1
+            // 
+            this.winChartViewer1.Location = new System.Drawing.Point(171, 35);
+            this.winChartViewer1.Name = "winChartViewer1";
+            this.winChartViewer1.Size = new System.Drawing.Size(1229, 480);
+            this.winChartViewer1.TabIndex = 0;
+            this.winChartViewer1.TabStop = false;
+            this.winChartViewer1.ViewPortChanged += new ChartDirector.WinViewPortEventHandler(this.winChartViewer1_ViewPortChanged);
+            this.winChartViewer1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.winChartViewer1_MouseDown);
+            this.winChartViewer1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.winChartViewer1_MouseMove);
+            this.winChartViewer1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.winChartViewer1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // DrawFrameOnRotate
+            // 
+            this.DrawFrameOnRotate.AutoSize = true;
+            this.DrawFrameOnRotate.Location = new System.Drawing.Point(12, 4);
+            this.DrawFrameOnRotate.Name = "DrawFrameOnRotate";
+            this.DrawFrameOnRotate.Size = new System.Drawing.Size(231, 25);
+            this.DrawFrameOnRotate.TabIndex = 2;
+            this.DrawFrameOnRotate.Text = "Draw Frame On Rotate";
+            this.DrawFrameOnRotate.UseVisualStyleBackColor = true;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 524);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(1438, 30);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // PointerPB
+            // 
+            this.PointerPB.AutoSize = true;
+            this.PointerPB.Location = new System.Drawing.Point(13, 104);
+            this.PointerPB.Name = "PointerPB";
+            this.PointerPB.Size = new System.Drawing.Size(93, 25);
+            this.PointerPB.TabIndex = 4;
+            this.PointerPB.Text = "Pointer";
+            this.PointerPB.UseVisualStyleBackColor = true;
+            this.PointerPB.CheckedChanged += new System.EventHandler(this.pointerPB_CheckedChanged);
+            // 
+            // zoomInPB
+            // 
+            this.zoomInPB.AutoSize = true;
+            this.zoomInPB.Location = new System.Drawing.Point(12, 136);
+            this.zoomInPB.Name = "zoomInPB";
+            this.zoomInPB.Size = new System.Drawing.Size(119, 25);
+            this.zoomInPB.TabIndex = 5;
+            this.zoomInPB.TabStop = true;
+            this.zoomInPB.Text = "zoomInPB";
+            this.zoomInPB.UseVisualStyleBackColor = true;
+            this.zoomInPB.CheckedChanged += new System.EventHandler(this.zoomInPB_CheckedChanged);
+
+            // 
+            // zoomOutPB
+            // 
+            this.zoomOutPB.AutoSize = true;
+            this.zoomOutPB.Location = new System.Drawing.Point(12, 168);
+            this.zoomOutPB.Name = "zoomOutPB";
+            this.zoomOutPB.Size = new System.Drawing.Size(137, 25);
+            this.zoomOutPB.TabIndex = 6;
+            this.zoomOutPB.TabStop = true;
+            this.zoomOutPB.Text = "zoomOutPB";
+            this.zoomOutPB.UseVisualStyleBackColor = true;
+            this.zoomOutPB.CheckedChanged += new System.EventHandler(this.zoomOutPB_CheckedChanged);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1438, 554);
+            this.Controls.Add(this.zoomOutPB);
+            this.Controls.Add(this.zoomInPB);
+            this.Controls.Add(this.PointerPB);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.DrawFrameOnRotate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.winChartViewer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.winChartViewer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ChartDirector.WinChartViewer winChartViewer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox DrawFrameOnRotate;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.RadioButton PointerPB;
+        private System.Windows.Forms.RadioButton zoomInPB;
+        private System.Windows.Forms.RadioButton zoomOutPB;
     }
 }
 
--- ChartDirector로3DScatterPlot그리기/Form1.cs
+++ ChartDirector로3DScatterPlot그리기/Form1.cs
@@ -1,20 +1,279 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.IO;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Globalization;
 using System.Windows.Forms;
+using ChartDirector;
 
 namespace ChartDirector로3DScatterPlot그리기
 {
     public partial class Form1 : Form
     {
+        private double rotationAngle = 45;
+        private double elevationAngle = 30;
+        private bool openFileFlag = false;
+        private DataTable dt1;
+        private bool hasFinishedInitialization;
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            // Initialize the WinChartViewer
+            initChartViewer(winChartViewer1);
+
+            // Can handle events now
+            hasFinishedInitialization = true;
+
+        }
+        //
+        // Initialize the WinChartViewer
+        //
+        private void initChartViewer(WinChartViewer viewer)
+        { 
+           
+            // Enable mouse wheel zooming by setting the zoom ratio to 1.1 per wheel event
+            viewer.MouseWheelZoomRatio = 1.1;
+            // Initially set the mouse usage to "Pointer" mode (Drag to Scroll mode)
+            PointerPB.Checked = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Display the chart
+            winChartViewer1.updateViewPort(true, false);
+
+        }
+
+
+        //
+        // The ViewPortChanged event handler
+        //
+        private void winChartViewer1_ViewPortChanged(object sender, WinViewPortEventArgs e)
+        {
+            // In addition to updating the chart, we may also need to update other controls that
+            // changes based on the view port.
+            updateControls(winChartViewer1);
+
+
+
+            // Update the chart if necessary
+            if (e.NeedUpdateChart)
+                drawChart(winChartViewer1);
+        }
+
+        //
+        // Update controls when the view port changed
+        //
+        private void updateControls(WinChartViewer viewer)
+        {
+           
+            // Update the scroll bar to reflect the view port position and width of the view port.
+            hScrollBar1.Enabled = winChartViewer1.ViewPortWidth < 1;
+            hScrollBar1.LargeChange = (int)Math.Ceiling(winChartViewer1.ViewPortWidth *
+                (hScrollBar1.Maximum - hScrollBar1.Minimum));
+            hScrollBar1.SmallChange = (int)Math.Ceiling(hScrollBar1.LargeChange * 0.1);
+            hScrollBar1.Value = (int)Math.Round(winChartViewer1.ViewPortLeft *
+                (hScrollBar1.Maximum - hScrollBar1.Minimum)) + hScrollBar1.Minimum;
+        }
+
+
+        public void drawChart(WinChartViewer viewer)
+        {
+            if (openFileFlag == false)
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    openFileFlag = true;
+                    string filename = openFileDialog1.FileName;
+                    dt1 = GetDataTableFromCsv(filename, false);
+                }
+            }
+                    dt1.Rows[0][0] = dt1.Rows[0][1];
+
+                    
+                    double[] xData = new double[dt1.Rows.Count * dt1.Columns.Count];
+                    double[] yData = new double[dt1.Rows.Count * dt1.Columns.Count];
+                    double[] zData = new double[dt1.Rows.Count * dt1.Columns.Count];
+
+                    int temp = 0;
+
+                    for (int x = 0; x < dt1.Rows.Count; x++)
+                    {
+                        for (int y = 0; y < dt1.Columns.Count; y++)
+                        {
+                            xData[temp] = x;
+                            yData[temp] = y;
+
+                            zData[temp] = Convert.ToInt64(dt1.Rows[x][y]);
+
+                            temp += 1;
+                        }
+                    }
+             
+                    // Create a SurfaceChart object of size 720 x 600 pixels
+                    SurfaceChart c = new SurfaceChart(720, 600);
+
+
+            
+            
+
+                    // Set the center of the plot region at (330, 290), and set width x depth x height to
+                    // 360 x 360 x 270 pixels
+                    c.setPlotRegion(330, 290, 360, 360, 270);
+
+                    // Set the data to use to plot the chart
+                    c.setData(xData, yData, zData);
+
+                    // Spline interpolate data to a 80 x 80 grid for a smooth surface
+                    c.setInterpolation(80, 80);
+             
+                // Set the view angles
+                c.setViewAngle(elevationAngle, rotationAngle);
+
+                // Check if draw frame only during rotation
+                if (isDragging && DrawFrameOnRotate.Checked)
+                    c.setShadingMode(Chart.RectangularFrame);
+
+                // Add a color axis (the legend) in which the left center is anchored at (660, 270). Set
+                // the length to 200 pixels and the labels on the right side.
+                c.setColorAxis(650, 270, Chart.Left, 200, Chart.Right);
+
+                // Set the x, y and z axis titles using 10 points Arial Bold font
+                c.xAxis().setTitle("X", "Arial Bold", 15);
+                c.yAxis().setTitle("Y", "Arial Bold", 15);
+
+                // Set axis label font
+                c.xAxis().setLabelStyle("Arial", 10);
+                c.yAxis().setLabelStyle("Arial", 10);
+                c.zAxis().setLabelStyle("Arial", 10);
+                c.colorAxis().setLabelStyle("Arial", 10);
+
+                // Output the chart
+                viewer.Chart = c;
+            }
+
+        
+
+        // keep track of the last mouse position to compute mouse movement
+        private int lastMouseX = -1;
+        private int lastMouseY = -1;
+        private bool isDragging = false;
+
+        private void winChartViewer1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (0 != (e.Button & MouseButtons.Left))
+            {
+                // Start Drag
+                isDragging = true;
+                lastMouseX = e.X;
+                lastMouseY = e.Y;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+
+        private void winChartViewer1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // The chart is configured to rotate by 90 degrees when the mouse moves from 
+                // left to right, which is the plot region width (360 pixels). Similarly, the
+                // elevation changes by 90 degrees when the mouse moves from top to buttom,
+                // which is the plot region height (270 pixels).
+                rotationAngle += (lastMouseX - e.X) * 90.0 / 360;
+                elevationAngle += (e.Y - lastMouseY) * 90.0 / 270;
+                lastMouseX = e.X;
+                lastMouseY = e.Y;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+
+        private void winChartViewer1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (0 != (e.Button & MouseButtons.Left))
+            {
+                // End Drag
+                isDragging = false;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            // When the view port is changed (user drags on the chart to scroll), the scroll bar will get
+            // updated. When the scroll bar changes (eg. user drags on the scroll bar), the view port will
+            // get updated. This creates an infinite loop. To avoid this, the scroll bar can update the 
+            // view port only if the view port is not updating the scroll bar.
+            if (hasFinishedInitialization && !winChartViewer1.IsInViewPortChangedEvent)
+            {
+                // Set the view port based on the scroll bar
+                winChartViewer1.ViewPortLeft = ((double)(hScrollBar1.Value - hScrollBar1.Minimum))
+                    / (hScrollBar1.Maximum - hScrollBar1.Minimum);
+
+                // Trigger a view port changed event to update the chart
+                winChartViewer1.updateViewPort(true, false);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileFlag = false;
+          
+        }
+
+        //
+        // Zoom In button event handler
+        //
+        private void zoomInPB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+                winChartViewer1.MouseUsage = WinChartMouseUsage.ZoomIn;
+        }
+
+        //
+        // Zoom Out button event handler
+        //
+        private void zoomOutPB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+                winChartViewer1.MouseUsage = WinChartMouseUsage.ZoomOut;
+        }
+
+        //
+        // Pointer (Drag to Scroll) button event handler
+        //
+        private void pointerPB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+                winChartViewer1.MouseUsage = WinChartMouseUsage.ScrollOnDrag;
+        }
+
+        static DataTable GetDataTableFromCsv(string path, bool isFirstRowHeader)
+        {
+            string header = isFirstRowHeader ? "Yes" : "No";
+
+            string pathOnly = Path.GetDirectoryName(path);
+            string fileName = Path.GetFileName(path);
+
+            string sql = @"SELECT * FROM [" + fileName + "]";
+
+            using (OleDbConnection connection = new OleDbConnection(
+                      @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathOnly +
+                      ";Extended Properties=\"Text;HDR=" + header + "\""))
+            using (OleDbCommand command = new OleDbCommand(sql, connection))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            {
+                DataTable dataTable = new DataTable();
+                dataTable.Locale = CultureInfo.CurrentCulture;
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
         }
     }
+
+
+
 }
